89495f0b759a2b7d3789ff0390014871b07dbe78
Andrzej Gołąbek <37544193+AndrzejHarnas@users.noreply.github.com>
2018-10-11 14:46:20 +0200

    Update data.js

--- src/components/data.js
+++ src/components/data.js
@@ -4,30 +4,33 @@ class userData extends React.Component {
 	
 	constructor(){
 		super();
-		this.state ={counter: 0};
+		this.state ={counter: 0, name:'noname', surname:'noname'};
 	}
 
 	render() {
-		const {firstName="NONAME", lastName="NONAME", age=0} = this.props
+		const {firstName='NONAME', lastName='NONAME', age=0} = this.props
 		
 		return(
-		
+//dodano nowe funkcjie i dwa inputy i dwa outputy		
 		<div>
 		<p>Witaj: {firstName} {lastName} masz: {age} lat </p>
 		<button onClick={this.buttonClick}> Kliknij </button>
-		
 		<button onClick={this.add.bind(this)}> + </button>
 		<output>{this.state.counter}</output>
 		<button onClick={this.minus.bind(this)}> - </button>
-		
+		<br/>
+		<p>Podaj imię:</p>
+		<input type='text' onChange={this.writeName.bind(this)} id='input1'></input> <output id='out1'> {this.state.name} </output>
+		<br/> 
+		<p>Podaj Nazwisko: </p>
+		<input type='text' onChange={this.writeSurname.bind(this)} id='input2'></input> <output id='out2'>  {this.state.surname} </output>
 		</div>
-		
 		);	
+		
 	}
 	
 	buttonClick(){
-		
-     alert("Cześć");
+     alert('Cześć');
 		
 	}
 	add(){
@@ -38,6 +41,20 @@ class userData extends React.Component {
 	minus(){
 		this.setState(
 		{counter: this.state.counter -1})
+	}
+
+	writeName(){
+		var x = document.getElementById('input1').value; 
+		
+		this.setState(
+		{name: x}
+		)
+	}
+	
+	writeSurname(){
+		this.setState(
+		{surname: document.getElementById('input2').value }
+		)
 	}
 	
 }
